6175eeecee80b5d3ea7d04dc3ed45d7bc9061919
kruzelgo <kruzelgo@gmail.com>
2024-04-14 11:17:07 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,12 @@ import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [filter, setFilter] = useState('');
@@ -21,8 +26,8 @@ export const App = () => {
   const handleSubmit = event => {
     event.preventDefault();
 
-    if (name.trim() === '') {
-      alert('Please enter a valid name.');
+    if (name.trim() === '' || number.trim() === '') {
+      alert('Please enter a valid name and number.');
       return;
     }
 
@@ -32,9 +37,24 @@ export const App = () => {
       number,
     };
 
+    if (
+      contacts.some(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
     setContacts(prevContacts => [...prevContacts, newContact]);
     setName('');
     setNumber('');
+  };
+
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
 
   const handleFilterChange = event => {
@@ -68,7 +88,10 @@ export const App = () => {
       />
       <h2>Contacts</h2>
       <Filter value={filter} onChange={handleFilterChange} />
-      <ContactList contacts={filteredContacts} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import css from './ContactList.module.css';
 
-const ContactList = ({ contacts }) => (
+// const ContactList = ({ contacts }) => (
+//   <ul className={css.contactList}>
+//     {contacts.map(contact => (
+//       <li key={contact.id}>
+//         {contact.name}: {contact.number}
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+const ContactList = ({ contacts, onDeleteContact }) => (
   <ul className={css.contactList}>
     {contacts.map(contact => (
       <li key={contact.id}>
         {contact.name}: {contact.number}
+        <button
+          onClick={() => onDeleteContact(contact.id)}
+          className={css.buttonDelete}
+        >
+          Delete
+        </button>
       </li>
     ))}
   </ul>
